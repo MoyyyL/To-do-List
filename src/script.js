e466ed62604769509a9407ced58da13f9b06bcc0
MoyyyL <moyyyl0526@gmail.com>
2025-07-17 15:28:20 -0600

    html, css, and javascript updated the project render and styles

--- src/script.js
+++ src/script.js
@@ -16,17 +16,34 @@ class domProjectsManager {
             const project = document.createElement("div");
             project.classList.add("todo__project");
             project.dataset.id = obj.id
-            //todo EDITAR EL INNER HTML POR CREATE ELEMENTS
-            project.innerHTML = `
-                <button class="project__button"></button>
-                <h2>${obj.name}</h2>
-                <ul class="project__list">
-                    <li class="to-do">
-                        <input type="checkbox" id="1"> //! neeed acces to individual id
-                        <label for="1">Opcion 1</label> //!
-                    </li>
-                </ul>
-            `;
+            
+            const projectOpen = document.createElement("button");
+            projectOpen.classList.add("project__button")
+
+            const projectTitle = document.createElement("h2");
+            projectTitle.textContent = obj.name;
+
+            const projectHowMuchCont = document.createElement("p");
+            projectHowMuchCont.textContent = `${obj.length} tasks`;
+
+            project.appendChild(projectOpen);
+            project.appendChild(projectTitle);
+            project.appendChild(projectHowMuchCont);
+
+            projectsWrapper.appendChild(project);
+            
+            //todo Crear un texto que diga "n# elementos...." dependiendo del lenght del whole y evitar el listado que de por si no tiene funcionalidad
+
+            //project.innerHTML = `
+            //    <button class="project__button"></button>
+            //    <h2>${obj.name}</h2>
+            //    <ul class="project__list">
+            //        <li class="to-do">
+            //            <input type="checkbox" id="1"> //! neeed acces to individual id
+            //            <label for="1">Opcion 1</label> //!
+            //        </li>
+            //    </ul>
+            //`;
 
             projectsWrapper.appendChild(project);
         })
@@ -42,3 +59,6 @@ class domProjectsManager {
         this.renderProjects();
     }
 }
+
+const elpepe = new domProjectsManager();
+elpepe.addProject("añai");
