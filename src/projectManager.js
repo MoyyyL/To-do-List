e6237aa30c8ad0b01a6f49922d44e682b8b4bb77
MoyyyL <moyyyl0526@gmail.com>
2025-07-18 09:25:28 -0600

    modularized the projectManager class into its own file

---
+++ src/projectManager.js
@@ -0,0 +1,58 @@
+import { ManageProjects } from "./classLogic";
+
+class domProjectsManager {
+    constructor() {
+        this.projectManager = new ManageProjects();
+        this.domAdding();
+    }
+
+    renderProjects() {
+        const whole = this.projectManager.getAllProjects();
+        const projectsWrapper = document.querySelector(".todo__projects-wrapper");
+        const dialog = document.querySelector(".project__dialog")
+        projectsWrapper.innerHTML = " ";
+        
+        whole.forEach(obj => {
+            const project = document.createElement("div");
+            project.classList.add("todo__project");
+            project.dataset.id = obj.id
+            
+            const projectOpen = document.createElement("button");
+            projectOpen.classList.add("project__button");
+            projectOpen.addEventListener("click", () => dialog.showModal());
+
+            const projectTitle = document.createElement("h2");
+            projectTitle.textContent = obj.name;
+
+            const projectHowMuchCont = document.createElement("p");
+            projectHowMuchCont.textContent = `${obj.length} tasks`;
+
+            project.appendChild(projectOpen);
+            project.appendChild(projectTitle);
+            project.appendChild(projectHowMuchCont);
+
+            projectsWrapper.appendChild(project);
+        })
+    }
+
+    addProject(name) {
+        this.projectManager.createProject(name);
+        this.renderProjects();
+    }
+
+    delProject(id) {
+        this.projectManager.delProyect(id);
+        this.renderProjects();
+    }
+
+    domAdding() {
+        const addButton = document.querySelector(".create-project");
+        const nameInput = document.querySelector(".project-name");
+        addButton.addEventListener("click", () => {
+            const name = nameInput.value;
+            this.addProject(name);
+        })
+    }
+}
+
+export {domProjectsManager}
